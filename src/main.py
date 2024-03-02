d6d7552dca8879cf9a80eb49a64a297cd055483c
Sara Azevedo <sarasazevedoo@gmail.com>
2024-03-02 12:04:35 +0000

    Added Menu

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-# Example file showing a circle moving on screen
 import pygame
 
 # pygame setup
@@ -8,7 +7,16 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+#Game Name 
+game_name = "SOKOBOND"
+
+#Fonts
+game_name_font = pygame.font.SysFont("Arial", 100)
+menu_options_font = pygame.font.SysFont("Arial", 24)
+
+# The options in the menu
+menu_options = ["start game", "settings", "about", "quit"]
+menu_option_selected = 0
 
 while running:
     # poll for events
@@ -16,21 +24,38 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN: #Options are controlled only by w and s 
+            if event.key == pygame.K_w:
+                menu_option_selected = (menu_option_selected - 1) % len(menu_options)
+            elif event.key == pygame.K_s:
+                menu_option_selected = (menu_option_selected + 1) % len(menu_options)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RETURN: #Press Enter
+                if menu_options[menu_option_selected] == "start game": #Start the game
+                    pass
+                if menu_options[menu_option_selected] == "settings": #Open settings
+                    pass
+                if menu_options[menu_option_selected] == "about": #Show settings
+                    pass
+                if menu_options[menu_option_selected] == "quit": #Quit the game
+                    running = False
 
+    
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    screen.fill("grey")
+    
+    #Draw the game name
+    game_name_surface = game_name_font.render(game_name, True, "black")
+    game_name_pos = game_name_surface.get_rect(midtop = (screen.get_width() // 2, screen.get_height() // 2 - 200))        
+    screen.blit( game_name_surface,game_name_pos) 
+    
+    #Draw Menu
+    for i, option in enumerate(menu_options):
+        option = menu_options_font.render(option, True, "black")
+        highlighted = option.get_rect(midtop = (screen.get_width() // 2, screen.get_height() // 2 + i* 50))
+        if i == menu_option_selected:
+            pygame.draw.rect(screen, "orange", highlighted.inflate(20,10)) # The rectangle shows in the option selected 
+        screen.blit(option,highlighted) 
 
     # flip() the display to put your work on screen
     pygame.display.flip()
