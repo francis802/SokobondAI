149aa9ae609f1af76dd4999e1ab33d371e918545
Henrique Pinheiro <heenriquepinheiro1@gmail.com>
2024-03-25 02:28:25 +0000

    proximity measure functions

--- src/algorythms.py
+++ src/algorythms.py
@@ -24,3 +24,57 @@ def evaluation(game):
         totalElectrons += piece.avElectrons
     
     return molecules * (totalElectrons + 1)
+
+
+def distance(piece1, piece2):
+    return abs(piece1.position[0] - piece2.position[0]) + abs(piece1.position[1] - piece2.position[1])
+
+def noWallBetween(game, piece1, piece2):
+    # print("Checking: ", piece1.position, piece2.position)
+    if piece1.position[0] == piece2.position[0]:
+        for i in range(min(piece1.position[1], piece2.position[1]), max(piece1.position[1], piece2.position[1])):
+            print("Checking: ", (piece1.position[0], i))
+            if (piece1.position[0], i) in game.walls:
+                return False
+    if piece1.position[1] == piece2.position[1]:
+        for i in range(min(piece1.position[0], piece2.position[0]), max(piece1.position[0], piece2.position[0])):
+            print("Checking: ", (i, piece1.position[1]))
+            if (i, piece1.position[1]) in game.walls:
+                return False
+    # print("No wall between")
+    # print ("------------------------------------------")
+    return True
+
+
+# This function will return the minimum distance between the molecule and the nearst atom
+def proximityMeasure(game):
+    # See in connections of the pivot atom whish has the minimum distance to some atom
+    minDistance = 100000000
+    pivot = game.pieces[0]
+    molecule = pivot
+    near = pivot
+
+    for piece in game.pieces:
+            if piece != pivot and noWallBetween(game, pivot, piece) and piece not in pivot.connections and pivot.avElectrons > 0:
+                temp = min(minDistance, distance(pivot, piece))
+                if temp < minDistance:
+                    minDistance = temp
+                    near = piece
+
+
+    if len(pivot.connections) != 0:    
+        for piece1 in pivot.connections:
+            for piece2 in game.pieces:
+                if piece2 != pivot and piece2 not in pivot.connections and noWallBetween(game, piece1, piece2) and piece1.avElectrons > 0:
+                    temp = min(minDistance, distance(piece1, piece2))
+                    if temp < minDistance:
+                        minDistance = temp
+                        near = piece2
+                        molecule = piece1
+    
+    print("Molecule: ", molecule.position)
+    print("Near: ", near.position)
+    
+    return minDistance - 1
+
+   
